c037ef6afbbb9234d2146da1a72c464e780d79c3
Dźwiedź <kg67199@gmail.com>
2020-09-26 20:54:23 +0200

    Add the missing metadata (and fix a crash)

--- src/main/java/dzwdz/deadtotems/mixin/LivingEntityMixin.java
+++ src/main/java/dzwdz/deadtotems/mixin/LivingEntityMixin.java
@@ -3,24 +3,17 @@ package dzwdz.deadtotems.mixin;
 import dzwdz.deadtotems.DeadTotems;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-    @Shadow
-    public void setStackInHand(Hand hand, ItemStack stack) {}
-
     @Inject(
             at = @At(
                     value = "INVOKE",
@@ -33,7 +26,7 @@ public class LivingEntityMixin {
     public void totemHook(DamageSource damageSource, CallbackInfoReturnable<Boolean> cir, ItemStack totemCopy, ItemStack totemOriginal, Hand var4[], int var5, int var6, Hand hand) {
         ItemStack brokenTotem = new ItemStack(DeadTotems.DEAD_TOTEM);
         if (totemOriginal.isEmpty()) {
-            setStackInHand(hand, brokenTotem);
+            ((LivingEntity)(Object)this).setStackInHand(hand, brokenTotem);
         } else if (((Object)this) instanceof ServerPlayerEntity) {
             ((ServerPlayerEntity)(Object)this).giveItemStack(brokenTotem);
         }
