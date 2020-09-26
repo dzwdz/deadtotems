cbf33f77f0e6681913588f1ff47c082fd61647bd
Dźwiedź <kg67199@gmail.com>
2020-09-26 20:00:53 +0200

    Dead Totems disappear on death

---
+++ src/main/java/dzwdz/deadtotems/mixin/PlayerEntityMixin.java
@@ -0,0 +1,31 @@
+package dzwdz.deadtotems.mixin;
+
+import dzwdz.deadtotems.DeadTotems;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin {
+    @Shadow @Final public PlayerInventory inventory;
+
+    @Inject(
+            at = @At("TAIL"),
+            method = "vanishCursedItems"
+    )
+    public void vanishDeadTotems(CallbackInfo callbackInfo) {
+        for(int i = 0; i < inventory.size(); ++i) {
+            ItemStack itemStack = inventory.getStack(i);
+            if (!itemStack.isEmpty() && itemStack.getItem() == DeadTotems.DEAD_TOTEM) {
+                inventory.removeStack(i);
+            }
+        }
+    }
+}
